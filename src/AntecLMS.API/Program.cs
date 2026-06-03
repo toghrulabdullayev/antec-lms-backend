765ad07e2aacdc6541b9bf4394e5f6ad3cf49d30
toghrulabdullayev <toghrulabdullayev1@gmail.com>
2026-06-04 01:01:59 +0400

    chore(api): format root file

--- src/AntecLMS.API/Program.cs
+++ src/AntecLMS.API/Program.cs
@@ -4,9 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build(
-  
-);
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
